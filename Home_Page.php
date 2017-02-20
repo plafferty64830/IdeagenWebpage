1d81067996adce6f9a46ec17e331d33dd36ae7a4
plafferty64830 <pat.lafferty@btinternet.com>
2017-02-20 23:59:20 +0000

    inital commit: php home page with some edits + stylesheet

---
+++ Home_Page.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html>
+
+<?php
+ echo "PHP code goes here";
+ ?>
+
+<head>
+
+<meta charset="utf-8">
+
+<title> Ideagen File Management System </title>
+
+<link rel = "stylesheet"
+   type = "text/css"
+   href = "Styles.css" />
+
+</head>
+
+<body>
+
+<div class="container">
+
+<header>
+   <h1>City Gallery</h1>
+</header>
+
+<nav>
+  <ul>
+
+
+     <li><a href="Home_Page.php"> <img src="images/home.png" alt="Home icon"> </a></li>
+     <li><a href="Use_Manage.php"> <img src="images/userManage.png" alt="User Management Icon"> </a></li>
+    <li><a href="#">Tokyo</a></li>
+  </ul>
+</nav>
+
+<article>
+  <h1>London</h1>
+  <p>London is the capital city of England. It is the most populous city in the  United Kingdom, with a metropolitan area of over 13 million inhabitants.</p>
+  <p>Standing on the River Thames, London has been a major settlement for two millennia, its history going back to its founding by the Romans, who named it Londinium.</p>
+</article>
+
+<footer>Copyright &copy; W3Schools.com</footer>
+
+</div>
+
+</body>
+</html>
